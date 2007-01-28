07b72d9e4cddede853aca0d658de362c7c2badba
deanhiller <deanhiller@d8937260-d917-0410-8237-ad64d1a5881f>
2007-01-28 01:13:01 +0000

    renaming should fix the build.  I can't tell as when I run testall, the build seems to hang forever.  See if this works better checking it in as that seemed to send out email while my builds on my machine seem to hang.  I think the build is now trying to pull these in as tests and they are not tests....my previous bad naming here.

---
+++ test/resources/walls/testB/mod/modB/TrialBModuleB.java
@@ -0,0 +1,9 @@
+
+package mod.modB;
+
+import mod.modA.TrialBModuleA;
+
+public class TrialBModuleB
+{
+    private TrialBModuleA temp;
+};
---
+++ test/resources/walls/testC/mod/TrialCModule.java
@@ -0,0 +1,11 @@
+
+package mod;
+
+import mod.modA.TrialCModuleA;
+import mod.modB.TrialCModuleB;
+
+public class TrialCModule
+{
+    private TrialCModuleA temp1;
+    private TrialCModuleB temp2;
+};
