3d6b8e70982c938d330c8f85f1644d372678b96f
mattinger <mattinger@d8937260-d917-0410-8237-ad64d1a5881f>
2008-06-16 17:38:26 +0000

    Adding in Ivy 2.0 support, but maintaining backward compatibility with Ivy 1.4.1

--- src/main/java/net/sf/antcontrib/net/URLImportTask.java
+++ src/main/java/net/sf/antcontrib/net/URLImportTask.java
@@ -16,9 +16,6 @@
 package net.sf.antcontrib.net;
 
 import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.net.MalformedURLException;
 import java.net.URL;
 
 import org.apache.tools.ant.BuildException;
@@ -27,9 +24,6 @@ import org.apache.tools.ant.Project;
 import org.apache.tools.ant.taskdefs.Expand;
 import org.apache.tools.ant.taskdefs.ImportTask;
 import org.apache.tools.ant.types.FileSet;
-
-import fr.jayasoft.ivy.ant.IvyCacheFileset;
-import fr.jayasoft.ivy.ant.IvyConfigure;
 
 /***
  * Task to import a build file from a url.  The build file can be a build.xml,
@@ -106,105 +100,72 @@ public class URLImportTask
 		throw new BuildException("'file' property not accessed for ImportURL.");
 	}
 
+	public String getOrg() {
+		return org;
+	}
+
+	public String getModule() {
+		return module;
+	}
+
+	public String getRev() {
+		return rev;
+	}
+
+	public String getConf() {
+		return conf;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public String getRepositoryUrl() {
+		return repositoryUrl;
+	}
+
+	public File getRepositoryDir() {
+		return repositoryDir;
+	}
+
+	public URL getIvyConfUrl() {
+		return ivyConfUrl;
+	}
+
+	public File getIvyConfFile() {
+		return ivyConfFile;
+	}
+
+	public String getResource() {
+		return resource;
+	}
+
+	public String getArtifactPattern() {
+		return artifactPattern;
+	}
+
+	public String getIvyPattern() {
+		return ivyPattern;
+	}
+
 	public void execute()
 		throws BuildException {
 		
-		IvyConfigure configure = new IvyConfigure();
-		configure.setProject(getProject());
-		configure.setLocation(getLocation());
-		configure.setOwningTarget(getOwningTarget());
-		configure.setTaskName(getTaskName());
-		configure.init();
-		if (ivyConfUrl != null) {
-			if (ivyConfUrl.getProtocol().equalsIgnoreCase("file")) {
-				String path = ivyConfUrl.getPath();
-				File f = new File(path);
-				if (! f.isAbsolute()) {
-					f = new File(getProject().getBaseDir(), path);
-				}
-				configure.setFile(f);
-			}
-			else {
-				try {
-					configure.setUrl(ivyConfUrl.toExternalForm());
-				}
-				catch (MalformedURLException e) {
-					throw new BuildException(e);
-				}
-			}
+		IvyAdapter adapter = null;
+		
+		try {
+			Class.forName("org.apache.ivy.Ivy");
+			adapter = new Ivy20Adapter();
 		}
-		else if (ivyConfFile != null) {
-			configure.setFile(ivyConfFile);
-		}
-		else if (repositoryDir != null ||
-				 repositoryUrl != null) {
-			File temp = null;
-			FileWriter fw = null;
-			
-			try {
-				temp = File.createTempFile("ivyconf", ".xml");
-				temp.deleteOnExit();
-				fw = new FileWriter(temp);
-				fw.write("<ivyconf>");
-				fw.write("<conf defaultResolver=\"default\" />");
-				fw.write("<resolvers>");
-				if (repositoryDir != null) {
-					fw.write("<filesystem name=\"default\">");
-					fw.write("<ivy pattern=\"" + repositoryDir + "/" + ivyPattern + "\"  />");
-					fw.write("<artifact pattern=\"" + repositoryDir + "/" + artifactPattern + "\"  />");
-					fw.write("</filesystem>");
-				}
-				else {
-					fw.write("<url name=\"default\">");
-					fw.write("<ivy pattern=\"" + repositoryUrl + "/" + ivyPattern + "\"  />");
-					fw.write("<artifact pattern=\"" + repositoryUrl + "/" + artifactPattern + "\"  />");
-					fw.write("</url>");
-				}
-				fw.write("</resolvers>");
-	
-				fw.write("<latest-strategies>");
-				fw.write("<latest-revision name=\"latest\"/>");
-				fw.write("</latest-strategies>");
-				fw.write("</ivyconf>");
-				fw.close();
-				fw = null;
-				
-				configure.setFile(temp);
-			}
-			catch (IOException e) {
-				throw new BuildException(e);
-			}
-			finally {
-				try {
-					if (fw != null) {
-						fw.close();
-						fw = null;
-					}
-				}
-				catch (IOException e) {
-					;
-				}
-			}
+		catch (ClassNotFoundException e) {
+			adapter = new Ivy14Adapter();
 		}
 		
-		configure.execute();
-
-		IvyCacheFileset cacheFileSet = new IvyCacheFileset();
-		cacheFileSet.setProject(getProject());
-		cacheFileSet.setLocation(getLocation());
-		cacheFileSet.setOwningTarget(getOwningTarget());
-		cacheFileSet.setTaskName(getTaskName());
-		cacheFileSet.setInline(true);
-		cacheFileSet.setOrganisation(org);
-		cacheFileSet.setModule(module);
-		cacheFileSet.setRevision(rev);
-		cacheFileSet.setConf(conf);
-		cacheFileSet.init();
-		cacheFileSet.setSetid(org + module + rev + ".fileset");
-		cacheFileSet.execute();
+		String setId = org + "." + module + "." + rev + ".fileset";
+		adapter.configure(this);
+		adapter.fileset(this, setId);
 		
-		FileSet fileset =
-			(FileSet) getProject().getReference(org + module + rev + ".fileset");
+		FileSet fileset =(FileSet) getProject().getReference(setId);
 		
 		DirectoryScanner scanner =
 			fileset.getDirectoryScanner(getProject());
